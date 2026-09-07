4eabb6e041d850707cb8f0f9f54ea3024f8a84e1
Jhocel Duga <jhocelbduga@gmail.com>
2026-09-07 16:38:37 +0800

    Get book review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -71,8 +71,14 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;   // Retrieve ISBN from request parameters
+
+  if (books[isbn]) {
+      // Return only the reviews for the given ISBN
+      res.send(JSON.stringify(books[isbn].reviews, null, 4));
+  } else {
+      res.status(404).send(JSON.stringify({ message: "Book not found" }, null, 4));
+  }
 });
 
 module.exports.general = public_users;
